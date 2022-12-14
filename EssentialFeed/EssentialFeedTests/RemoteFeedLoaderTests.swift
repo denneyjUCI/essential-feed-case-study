9d5462d7f8df3ae4def36f483acd5cfd5c2009ac
Jonathan Denney <jonathan.denney1@gmail.com>
2022-12-14 09:36:46 -0600

    Extract helper to simplify test setup

--- EssentialFeed/EssentialFeedTests/RemoteFeedLoaderTests.swift
+++ EssentialFeed/EssentialFeedTests/RemoteFeedLoaderTests.swift
@@ -69,27 +69,16 @@ final class RemoteFeedLoaderTests: XCTestCase {
     func test_load_deliversNoItemsOn200ResponseAndEmptyJSONList() throws {
         let (sut, client) = makeSUT()
 
-        let item1 = FeedItem(id: UUID(),
-                             description: nil,
-                             location: nil,
+        let item1 = makeItem(id: UUID(),
                              imageURL: URL(string: "http://a-url.com")!)
-        let item1JSON = [
-            "id": item1.id.uuidString,
-            "image": item1.imageURL.absoluteString
-        ]
-        let item2 = FeedItem(id: UUID(),
+
+        let item2 = makeItem(id: UUID(),
                              description: "a description",
                              location: "a location",
                              imageURL: URL(string: "http://another-url.com")!)
-        let item2JSON = [
-            "id": item2.id.uuidString,
-            "description": item2.description,
-            "location": item2.location,
-            "image": item2.imageURL.absoluteString
-        ]
 
-        let itemsJSON = ["items": [item1JSON, item2JSON]]
-        expect(sut, toCompleteWithResult: .success([item1, item2]), when: {
+        let itemsJSON = ["items": [item1.json, item2.json]]
+        expect(sut, toCompleteWithResult: .success([item1.model, item2.model]), when: {
             let data = try! JSONSerialization.data(withJSONObject: itemsJSON)
             client.complete(withStatusCode: 200, data: data)
         })
@@ -120,6 +109,23 @@ final class RemoteFeedLoaderTests: XCTestCase {
         addTeardownBlock { [weak instance] in
             XCTAssertNil(instance, "Instance should have been deallocated, potential memory leak!", file: file, line: line)
         }
+    }
+
+    private func makeItem(id: UUID, description: String? = nil, location: String? = nil, imageURL: URL) -> (model: FeedItem, json: [String: Any]) {
+        let item = FeedItem(id: id,
+                             description: description,
+                             location: location,
+                             imageURL: imageURL)
+        let itemJSON = [
+            "id": id.uuidString,
+            "description": description,
+            "location": location,
+            "image": imageURL.absoluteString
+        ].reduce(into: [String: Any]()) { acc, e in
+            if let value = e.value { acc[e.key] = value }
+        }
+
+        return (model: item, json: itemJSON)
     }
 
     private class HTTPClientSpy: HTTPClient {
