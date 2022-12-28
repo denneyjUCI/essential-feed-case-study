39c508b37a91c731cc341ca41d3a8e1bf52acb97
Jonathan Denney <jonathan.denney1@gmail.com>
2022-12-28 16:43:19 -0600

    Does not message store upon creation

---
+++ EssentialFeed/EssentialFeedTests/Feed Cache/CacheFeedUseCaseTests.swift
@@ -0,0 +1,23 @@
+import XCTest
+
+class LocalFeedLoader {
+
+    init(store: FeedStore) {
+
+    }
+
+}
+
+class FeedStore {
+    var deleteCachedFeedCallCount = 0
+}
+
+final class CacheFeedUseCaseTests: XCTestCase {
+
+    func test_init_doesNotDeleteCachedFeed() {
+        let store = FeedStore()
+        _ = LocalFeedLoader(store: store)
+
+        XCTAssertEqual(store.deleteCachedFeedCallCount, 0)
+    }
+}
