de12d4e50680dbbf6007727baa9118732beefcdc
Jonathan Denney <jonathan.denney1@gmail.com>
2023-01-05 17:40:42 -0600

    Move presentable view models to their own files

---
+++ EssentialFeed/EssentialFeed/Feed Presentation/FeedLoadingViewModel.swift
@@ -0,0 +1,12 @@
+//
+//  FeedLoadingViewModel.swift
+//  EssentialFeed
+//
+//  Created by Jonathan Denney on 1/5/23.
+//
+
+import Foundation
+
+public struct FeedLoadingViewModel {
+    public let isLoading: Bool
+}
---
+++ EssentialFeed/EssentialFeed/Feed Presentation/FeedViewModel.swift
@@ -0,0 +1,12 @@
+//
+//  FeedViewModel.swift
+//  EssentialFeed
+//
+//  Created by Jonathan Denney on 1/5/23.
+//
+
+import Foundation
+
+public struct FeedViewModel {
+    public let feed: [FeedImage]
+}
