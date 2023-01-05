10e07ad5524624c874121b65841765b52f4cd5e4
Jonathan Denney <jonathan.denney1@gmail.com>
2023-01-05 12:13:45 -0600

    Move `FeedImageCell` view configuration to storyboard

--- EssentialFeed/EssentialFeediOS/Feed UI/Controllers/FeedImageCellController.swift
+++ EssentialFeed/EssentialFeediOS/Feed UI/Controllers/FeedImageCellController.swift
@@ -14,27 +14,29 @@ protocol FeedImageCellControllerDelegate {
 
 final class FeedImageCellController: FeedImageView {
     private let delegate: FeedImageCellControllerDelegate
-    private lazy var cell = FeedImageCell()
+    private var cell: FeedImageCell?
 
     init(delegate: FeedImageCellControllerDelegate) {
         self.delegate = delegate
     }
 
-    func view() -> UITableViewCell {
+    func view(in tableView: UITableView) -> UITableViewCell {
+        let cell = tableView.dequeueReusableCell(withIdentifier: "FeedImageCell") as! FeedImageCell
+        self.cell = cell
         delegate.didRequestImage()
         return cell
     }
 
     func display(_ viewModel: FeedImageViewModel<UIImage>) {
-        cell.locationContainer.isHidden = !viewModel.hasLocation
-        cell.locationLabel.text = viewModel.location
-        cell.descriptionLabel.text = viewModel.description
+        cell?.locationContainer.isHidden = !viewModel.hasLocation
+        cell?.locationLabel.text = viewModel.location
+        cell?.descriptionLabel.text = viewModel.description
 
-        cell.imageRetryButton.isHidden = !viewModel.shouldRetry
+        cell?.imageRetryButton.isHidden = !viewModel.shouldRetry
 
-        cell.feedImageContainer.isShimmering = viewModel.isLoading
-        cell.feedImageView.image = viewModel.image
-        cell.onRetry = delegate.didRequestImage
+        cell?.feedImageContainer.isShimmering = viewModel.isLoading
+        cell?.feedImageView.image = viewModel.image
+        cell?.onRetry = delegate.didRequestImage
     }
 
     func preload() {
