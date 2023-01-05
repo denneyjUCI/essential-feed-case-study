4f8b068b3c2383bce1d6b66f089e4776fb4e8528
Jonathan Denney <jonathan.denney1@gmail.com>
2023-01-05 16:26:50 -0600

    Move `FeedErrorViewModel` to separate file

---
+++ EssentialFeed/EssentialFeediOS/Feed Presentation/FeedErrorViewModel.swift
@@ -0,0 +1,12 @@
+//
+//  FeedErrorViewModel.swift
+//  EssentialFeediOS
+//
+//  Created by Jonathan Denney on 1/5/23.
+//
+
+import Foundation
+
+struct FeedErrorViewModel {
+    let message: String?
+}
