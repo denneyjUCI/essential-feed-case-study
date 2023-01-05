b0f411be3066828ea9a37ce882288be36884d384
Jonathan Denney <jonathan.denney1@gmail.com>
2023-01-05 10:17:35 -0600

    Remove `FeedImageViewModel` dependency on UIKit using a transformation closure and generics

--- EssentialFeed/EssentialFeediOS/Feed UI/Models/FeedImageViewModel.swift
+++ EssentialFeed/EssentialFeediOS/Feed UI/Models/FeedImageViewModel.swift
@@ -5,17 +5,18 @@
 //  Created by Jonathan Denney on 1/5/23.
 //
 
-import UIKit
+import Foundation
 import EssentialFeed
 
-final class FeedImageViewModel {
+final class FeedImageViewModel<Image> {
     typealias Observer<T> = (T) -> Void
 
     private var task: FeedImageDataLoaderTask?
     private let model: FeedImage
     private let imageLoader: FeedImageDataLoader
+    private let imageTransformer: (Data) -> Image?
 
-    var onImageChange: Observer<UIImage?>?
+    var onImageChange: Observer<Image>?
     var onShouldRetryImageLoadStateChange: Observer<Bool>?
     var onImageLoadingStateChange: Observer<Bool>?
 
@@ -31,9 +32,10 @@ final class FeedImageViewModel {
         model.description
     }
 
-    init(model: FeedImage, imageLoader: FeedImageDataLoader) {
+    init(model: FeedImage, imageLoader: FeedImageDataLoader, imageTransformer: @escaping (Data) -> Image?) {
         self.model = model
         self.imageLoader = imageLoader
+        self.imageTransformer = imageTransformer
     }
 
     func loadImage() {
@@ -45,7 +47,7 @@ final class FeedImageViewModel {
     }
 
     private func handle(_ result: FeedImageDataLoader.Result) {
-        if let image = (try? result.get()).flatMap(UIImage.init) {
+        if let image = (try? result.get()).flatMap(imageTransformer) {
             onImageChange?(image)
         } else {
             onShouldRetryImageLoadStateChange?(true)
