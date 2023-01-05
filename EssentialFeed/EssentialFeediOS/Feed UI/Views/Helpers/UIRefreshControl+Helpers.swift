59a9516ad61ad62a85d6621a6a2609f274e4f750
Jonathan Denney <jonathan.denney1@gmail.com>
2023-01-05 16:28:13 -0600

    Move refreshing logic to a helper extension

---
+++ EssentialFeed/EssentialFeediOS/Feed UI/Views/Helpers/UIRefreshControl+Helpers.swift
@@ -0,0 +1,14 @@
+//
+//  UIRefreshControl+Helpers.swift
+//  EssentialFeediOS
+//
+//  Created by Jonathan Denney on 1/5/23.
+//
+
+import UIKit
+
+extension UIRefreshControl {
+    func update(isRefreshing: Bool) {
+        isRefreshing ? beginRefreshing() : endRefreshing()
+    }
+}
