c28aca80e90337a44374a9349c8d09facc0447fe
Jonathan Denney <jonathan.denney1@gmail.com>
2023-01-05 17:44:58 -0600

    Replace `FeedPresenter` from iOS module with the `FeedPresenter` from the cross-platform `EssentialFeed` module

--- EssentialFeed/EssentialFeediOS/Feed UI/Composer/FeedLoaderAdapter.swift
+++ EssentialFeed/EssentialFeediOS/Feed UI/Composer/FeedLoaderAdapter.swift
@@ -6,6 +6,7 @@
 //
 
 import UIKit
+import EssentialFeed
 
 final class FeedViewAdapter: FeedView {
     private weak var controller: FeedViewController?
@@ -16,7 +17,7 @@ final class FeedViewAdapter: FeedView {
         self.imageLoader = imageLoader
     }
 
-    func display(_ viewModel: FeedViewModel) {
+    public func display(_ viewModel: FeedViewModel) {
         controller?.tableModel = viewModel.feed.map {  model in
             let adapter = FeedImageDataLoaderPresentationAdapter<WeakRefVirtualProxy<FeedImageCellController>, UIImage>(model: model, imageLoader: imageLoader)
             let view = FeedImageCellController(delegate: adapter)
