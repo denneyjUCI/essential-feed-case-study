85873a116f795d498b3141fec4643502874970d1
Jonathan Denney <jonathan.denney1@gmail.com>
2023-01-09 11:29:27 -0600

    Extract `FeedImageDataStoreSpy` to shared scope

---
+++ EssentialFeed/EssentialFeedTests/Feed Cache/Helpers/FeedImageDataStoreSpy.swift
@@ -0,0 +1,35 @@
+//
+//  FeedImageDataStoreSpy.swift
+//  EssentialFeedTests
+//
+//  Created by Jonathan Denney on 1/9/23.
+//
+
+import Foundation
+import EssentialFeed
+
+class FeedImageDataStoreSpy: FeedImageDataStore {
+    enum Message: Equatable {
+        case insert(data: Data, for: URL)
+        case retrieve(dataFor: URL)
+    }
+    var receivedMessages = [Message]()
+    private var completions = [(FeedImageDataStore.RetrievalResult) -> Void]()
+
+    func retrieve(dataForURL url: URL, completion: @escaping (FeedImageDataStore.RetrievalResult) -> Void) {
+        receivedMessages.append(.retrieve(dataFor: url))
+        completions.append(completion)
+    }
+
+    func complete(with error: Error, at index: Int = 0) {
+        completions[index](.failure(error))
+    }
+
+    func complete(with data: Data?, at index: Int = 0) {
+        completions[index](.success(data))
+    }
+
+    func insert(_ data: Data, for url: URL) {
+        receivedMessages.append(.insert(data: data, for: url))
+    }
+    }
--- EssentialFeed/EssentialFeedTests/Feed Cache/LoadImageDataFromCacheUseCaseTests.swift
+++ EssentialFeed/EssentialFeedTests/Feed Cache/LoadImageDataFromCacheUseCaseTests.swift
@@ -67,7 +67,7 @@ class LoadImageDataFromCacheUseCaseTests: XCTestCase {
     }
 
     func test_loadImageDataFromURL_doesNotDeliverResultAfterSUTInstanceDeallocation() {
-        let store = FeedStoreSpy()
+        let store = FeedImageDataStoreSpy()
         var sut: LocalFeedImageDataLoader? = LocalFeedImageDataLoader(store: store)
         let foundData = anyData()
 
@@ -93,8 +93,8 @@ class LoadImageDataFromCacheUseCaseTests: XCTestCase {
     }
 
     // MARK: - Helpers
-    private func makeSUT(file: StaticString = #filePath, line: UInt = #line) -> (sut: LocalFeedImageDataLoader, store: FeedStoreSpy) {
-        let store = FeedStoreSpy()
+    private func makeSUT(file: StaticString = #filePath, line: UInt = #line) -> (sut: LocalFeedImageDataLoader, store: FeedImageDataStoreSpy) {
+        let store = FeedImageDataStoreSpy()
         let sut = LocalFeedImageDataLoader(store: store)
         trackForMemoryLeaks(store, file: file, line: line)
         trackForMemoryLeaks(sut, file: file, line: line)
@@ -126,32 +126,6 @@ class LoadImageDataFromCacheUseCaseTests: XCTestCase {
 
     private func notFound() -> FeedImageDataLoader.Result {
         return .failure(LocalFeedImageDataLoader.LoadError.notFound)
-    }
-
-    private class FeedStoreSpy: FeedImageDataStore {
-        enum Message: Equatable {
-            case insert(data: Data, for: URL)
-            case retrieve(dataFor: URL)
-        }
-        var receivedMessages = [Message]()
-        private var completions = [(FeedImageDataStore.RetrievalResult) -> Void]()
-
-        func retrieve(dataForURL url: URL, completion: @escaping (FeedImageDataStore.RetrievalResult) -> Void) {
-            receivedMessages.append(.retrieve(dataFor: url))
-            completions.append(completion)
-        }
-
-        func complete(with error: Error, at index: Int = 0) {
-            completions[index](.failure(error))
-        }
-
-        func complete(with data: Data?, at index: Int = 0) {
-            completions[index](.success(data))
-        }
-
-        func insert(_ data: Data, for url: URL) {
-            receivedMessages.append(.insert(data: data, for: url))
-        }
     }
 
 }
