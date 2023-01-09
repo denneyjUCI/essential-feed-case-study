c035a3bb17e4115c800810d9ddfff148643ed8e1
Jonathan Denney <jonathan.denney1@gmail.com>
2023-01-09 13:56:13 -0600

    Add empty EssentialApp project

---
+++ EssentialApp/EssentialApp/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  EssentialApp
+//
+//  Created by Jonathan Denney on 1/9/23.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
