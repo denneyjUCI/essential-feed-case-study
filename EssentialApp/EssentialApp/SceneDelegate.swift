45f31dc8a2866abee6ccaebe1e1034f71ec80a0d
Jonathan Denney <jonathan.denney1@gmail.com>
2023-01-12 15:15:41 -0600

    Set up window in SceneDelegate

--- EssentialApp/EssentialApp/SceneDelegate.swift
+++ EssentialApp/EssentialApp/SceneDelegate.swift
@@ -35,8 +35,9 @@ class SceneDelegate: UIResponder, UIWindowSceneDelegate {
 
 
     func scene(_ scene: UIScene, willConnectTo session: UISceneSession, options connectionOptions: UIScene.ConnectionOptions) {
-        guard let _ = (scene as? UIWindowScene) else { return }
+        guard let scene = (scene as? UIWindowScene) else { return }
 
+        window = UIWindow(windowScene: scene)
         configureWindow()
     }
 
@@ -66,6 +67,7 @@ class SceneDelegate: UIResponder, UIWindowSceneDelegate {
                             cache: localImageLoader))
                 )
             )
+        window?.makeKeyAndVisible()
     }
 
 }
--- EssentialApp/EssentialAppTests/SceneDelegateTests.swift
+++ EssentialApp/EssentialAppTests/SceneDelegateTests.swift
@@ -4,6 +4,16 @@ import EssentialFeediOS
 
 final class SceneDelegateTests: XCTestCase {
 
+    func test_configureWindow_setsWindowAsKeyAndVisible() {
+        let window = UIWindowSpy()
+        let sut = SceneDelegate()
+        sut.window = window
+
+        sut.configureWindow()
+
+        XCTAssertEqual(window.makeKeyAndVisibleCallCount, 1, "Expected window to be key and visible")
+    }
+
     func test_configureWindow_configuresRootViewController()  {
         let sut = SceneDelegate()
         sut.window = UIWindow()
@@ -18,4 +28,11 @@ final class SceneDelegateTests: XCTestCase {
         XCTAssertTrue(topController is FeedViewController, "Expected a feed controller as top view controller, got \(String(describing: topController)) instead")
     }
 
+}
+
+private class UIWindowSpy: UIWindow {
+    var makeKeyAndVisibleCallCount = 0
+    override func makeKeyAndVisible() {
+        makeKeyAndVisibleCallCount += 1
+    }
 }
