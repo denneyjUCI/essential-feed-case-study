68ffa9d452a14545ecd15d3b711a28e86f0a0c19
Jonathan Denney <jonathan.denney1@gmail.com>
2023-01-13 15:53:31 -0600

    Add `ImageComment` data model

---
+++ EssentialFeed/EssentialFeed/Image Comments Feature/ImageComment.swift
@@ -0,0 +1,22 @@
+//
+//  Image Comments.swift
+//  EssentialFeed
+//
+//  Created by Jonathan Denney on 1/13/23.
+//
+
+import Foundation
+
+public struct ImageComment: Equatable {
+    public let id: UUID
+    public let message: String
+    public let createdAt: Date
+    public let username: String
+
+    public init(id: UUID, message: String, createdAt: Date, username: String) {
+        self.id = id
+        self.message = message
+        self.createdAt = createdAt
+        self.username = username
+    }
+}
