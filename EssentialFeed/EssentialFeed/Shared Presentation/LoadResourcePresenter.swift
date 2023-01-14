10d9ce92efc4cbda1a612e263e904ae2a5ddb810
Jonathan Denney <jonathan.denney1@gmail.com>
2023-01-13 19:48:52 -0600

    Duplicate FeedPresenter as LoadResourcePresenter

---
+++ EssentialFeed/EssentialFeed/Shared Presentation/LoadResourcePresenter.swift
@@ -0,0 +1,49 @@
+//
+//  LoadResourcePresenter.swift
+//  EssentialFeed
+//
+//  Created by Jonathan Denney on 1/13/23.
+//
+
+import Foundation
+
+public final class LoadResourcePresenter {
+    private let feedView: FeedView
+    private let loadingView: FeedLoadingView
+    private let errorView: FeedErrorView
+
+    public static var title: String {
+        return NSLocalizedString("FEED_VIEW_TITLE",
+                                 tableName: "Feed",
+                                 bundle: Bundle(for: FeedPresenter.self),
+                                 comment: "Title for feed view")
+    }
+
+    private var feedLoadError: String {
+        return NSLocalizedString("FEED_VIEW_CONNECTION_ERROR",
+                                 tableName: "Feed",
+                                 bundle: Bundle(for: FeedPresenter.self),
+                                 comment: "Title for feed view")
+    }
+
+    public init(feedView: FeedView, loadingView: FeedLoadingView, errorView: FeedErrorView) {
+        self.feedView = feedView
+        self.loadingView = loadingView
+        self.errorView = errorView
+    }
+
+    public func didStartLoadingFeed() {
+        errorView.display(.noError)
+        loadingView.display(FeedLoadingViewModel(isLoading: true))
+    }
+
+    public func didFinishLoadingFeed(with feed: [FeedImage]) {
+        feedView.display(FeedViewModel(feed: feed))
+        loadingView.display(FeedLoadingViewModel(isLoading: false))
+    }
+
+    public func didFinishLoadingFeed(with error: Error) {
+        errorView.display(.error(message: feedLoadError))
+        loadingView.display(FeedLoadingViewModel(isLoading: false))
+    }
+}
