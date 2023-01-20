415847c1f90ff0ce30fe22a4bb0b1d7a6c44ef7c
Jonathan Denney <jonathan.denney1@gmail.com>
2023-01-20 15:58:50 -0500

    Notifies selection handler on image selection

--- EssentialApp/EssentialApp/FeedViewAdapter.swift
+++ EssentialApp/EssentialApp/FeedViewAdapter.swift
@@ -12,10 +12,14 @@ import EssentialFeediOS
 final class FeedViewAdapter: ResourceView {
     private weak var controller: ListViewController?
     private let imageLoader: (URL) -> FeedImageDataLoader.Publisher
+    private let selection: (FeedImage) -> Void
 
-    public init(controller: ListViewController, imageLoader: @escaping (URL) -> FeedImageDataLoader.Publisher) {
+    public init(controller: ListViewController,
+                imageLoader: @escaping (URL) -> FeedImageDataLoader.Publisher,
+                selection: @escaping (FeedImage) -> Void) {
         self.controller = controller
         self.imageLoader = imageLoader
+        self.selection = selection
     }
 
     public func display(_ viewModel: FeedViewModel) {
@@ -25,7 +29,10 @@ final class FeedViewAdapter: ResourceView {
             })
             let view = FeedImageCellController(
                 viewModel: FeedImagePresenter.map(model),
-                delegate: adapter
+                delegate: adapter,
+                selection: { [selection] in
+                    selection(model)
+                }
             )
             adapter.presenter = LoadResourcePresenter(
                 resourceView: WeakRefVirtualProxy(view),
--- EssentialApp/EssentialAppTests/FeedUIIntegrationTests.swift
+++ EssentialApp/EssentialAppTests/FeedUIIntegrationTests.swift
@@ -13,6 +13,23 @@ class FeedUIIntegrationTests: XCTestCase {
         XCTAssertEqual(sut.title, feedTitle)
     }
 
+    func test_imageSelection_notifiesHandler() {
+        var selectedImages = [FeedImage]()
+        let image0 = makeImage()
+        let image1 = makeImage()
+        let (sut, loader) = makeSUT(selection: { selectedImages.append($0) })
+
+        sut.loadViewIfNeeded()
+
+        loader.completeFeedLoading(with: [image0, image1])
+
+        sut.simulateTapOnFeedImage(at: 0)
+        XCTAssertEqual(selectedImages, [image0])
+
+        sut.simulateTapOnFeedImage(at: 1)
+        XCTAssertEqual(selectedImages, [image0, image1])
+    }
+
     func test_loadFeedActions_requestsFeedFromLoader() {
         let (sut, loader) = makeSUT()
 
@@ -327,11 +344,15 @@ class FeedUIIntegrationTests: XCTestCase {
     }
 
     // MARK: - Helpers
-    private func makeSUT(file: StaticString = #filePath, line: UInt = #line) -> (sut: ListViewController, loader: LoaderSpy) {
+    private func makeSUT(selection: @escaping (FeedImage) -> Void = { _ in },
+                         file: StaticString = #filePath,
+                         line: UInt = #line
+    ) -> (sut: ListViewController, loader: LoaderSpy) {
         let loader = LoaderSpy()
         let sut = FeedUIComposer.feedComposedWith(
             feedLoader: loader.loadPublisher,
-            imageLoader: loader.loadImageDataPublisher
+            imageLoader: loader.loadImageDataPublisher,
+            selection: selection
         )
         trackForMemoryLeaks(loader, file: file, line: line)
         trackForMemoryLeaks(sut, file: file, line: line)
