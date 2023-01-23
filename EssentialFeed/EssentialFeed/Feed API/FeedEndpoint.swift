066e3f7d9249f2a0b9b11a5a21a38659151baf29
Jonathan Denney <jonathan.denney1@gmail.com>
2023-01-23 15:03:05 -0600

    Refactor to use id instead of FeedImage to avoid coupling endpoint to domain object

--- EssentialFeed/EssentialFeed/Feed API/FeedEndpoint.swift
+++ EssentialFeed/EssentialFeed/Feed API/FeedEndpoint.swift
@@ -5,18 +5,18 @@
 import Foundation
 
 public enum FeedEndpoint {
-    case get(after: FeedImage? = nil)
+    case get(after: UUID? = nil)
 
     public func url(baseURL: URL) -> URL {
         switch self {
-        case let .get(image):
+        case let .get(id):
             var components = URLComponents()
             components.scheme = baseURL.scheme
             components.host = baseURL.host
             components.path = baseURL.path + "/v1/feed"
             components.queryItems = [
                 URLQueryItem(name: "limit", value: "10"),
-                image.map { URLQueryItem(name: "after_id", value: $0.id.uuidString) }
+                id.map { URLQueryItem(name: "after_id", value: $0.uuidString) }
             ].compactMap { $0 }
             return components.url!
         }
